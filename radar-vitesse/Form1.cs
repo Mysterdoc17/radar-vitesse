bb6170cb5d4e2d9d646c617b98f76162485b9234
ratoujemie8617 <jeremie.baudon.17@gmail.com>
2022-04-14 23:53:05 +0200

    14042022

--- radar-vitesse/Form1.cs
+++ radar-vitesse/Form1.cs
@@ -1,40 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+namespace radar_vitesse;
 
-namespace radar_vitesse
+public partial class Form1 : Form
 {
-    public partial class Form1 : Form
+    public Form1()
     {
-        private TextBox textBox1;
-        public Form1()
-        {
-            Public getInitializeComponent() {
-                return InitializeComponent();
-            }
-        }
-        private void InitializeComponent() {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.SuspendLayout();
-            
-            // textBox1
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
+        InitializeComponent();
+    }
+    public void label1(object sender, EventArgs e)
+    {
+        Label label1 = new Label();
+        label1.Text = "Vitesse";
+        Controls.Add(label1);
+    }
 
-            // Form1
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.textBox1);
-            this.Name = "Form1";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-        }
+    public void textBox1(object sender, EventArgs e)
+    {
+        TextBox textBox1 = new TextBox();
+        textBox1.Text = "Saisir une vitesse";
+        Controls.Add(textBox1);
     }
 }
