88bc56a3bed4cbc170312021b245469a71669f18
ratoujemie8617 <baudonj@fenelonsio.local>
2022-05-09 16:41:44 +0200

    09052022

--- radarVitesse/radarVitesse/CalculVitesse.cs
+++ radarVitesse/radarVitesse/CalculVitesse.cs
@@ -1,6 +1,4 @@
-using System.Collections.Generic;  
-using System.IO;  
-using System.Threading.Tasks;
+
 
 namespace radarVitesse
 {
@@ -19,37 +17,25 @@ namespace radarVitesse
             if (temps_difference < 0) {
                 MessageBox.Show("Les saisies ne sont pas correctes");
             }
-        }
-        // instanciation de src1
-        Bitmap src1 = new Bitmap(int x, int y, PixelFormat pixelFormat);
-        public static Bitmap Diff(Bitmap src1, Bitmap src2, int x1, int y1, int x2, int y2, int width, int height){
-            Bitmap bmp = new Bitmap(int x, int y);
+            else {
+                /* calcul automatique de la vitesse
+                vitesse = temps_difference / 1000;
+                vitesseKmH = vitesse * 3.6;
+                */
+                
+                List<string> fileNames = new List<string> { "Input1.png", "Input2.png" };
+                
+                // récupération des données de temps avec les saisies utilisateur
+                int temps_depart_saisie = temps_depart;
+                int temps_arrivee_saisie = temps_arrivee;
 
-            Bitmap diffBM = new Bitmap(width, height, PixelFormat.Format24bppRgb);
+                // analyse de la différence des photos
+                int temps_difference_saisie = temps_arrivee_saisie - temps_depart_saisie;
 
-            for (int y = 0; y < height; y++) {
-                for (int x = 0; x < width; x++) {
-                    //Obtention des deux couleurs du pixel
-                    Color col1 = src1.GetPixel(x1 + x, y1 + y);
-                    Color col2 = src2.GetPixel(x2 + x, y2 + y);
+                // affichage de la vitesse
+                MessageBox.Show("Votre vitesse est de " + vitesseKmH + " km/h");
 
-                    // Obtention de la différence RVB
-                    int r = 0, g = 0, b = 0;
-                    r = Math.Abs(col1.R - col2.R);
-                    g = Math.Abs(col1.G - col2.G);
-                    b = Math.Abs(col1.B - col2.B);
-
-                    // Inversion de la moyenne des différences
-                    int dif = 255 - ((r+g+b) / 3);
-
-                    // Création d'une nouvelle couleur RVB en niveaux de gris
-                    Color newcol = Color.FromArgb(dif, dif, dif);
-
-                    //diffBM.SetPixel(x, y, newcol);
-                }
-            }
-
-            //return diffBM;
+            }    
         }
     }
 }
